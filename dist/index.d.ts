f0332f8843c4c2c458219ae7425ae147cdee0b2a
Mo Binni <mo@binni.io>
2024-12-16 15:20:18 -0500

    Patch entry for new arch

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const changeIcon: (iconName?: string) => Promise<string>;
-export declare const resetIcon: () => Promise<string>;
-export declare const getIcon: () => Promise<string>;
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-import { NativeModules } from "react-native";
-export const changeIcon = (iconName) => NativeModules.ChangeIcon.changeIcon(iconName);
-export const resetIcon = () => changeIcon();
-export const getIcon = () => NativeModules.ChangeIcon.getIcon();
